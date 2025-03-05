a7429ee130cc17fbd677f0c5bb517107ac61ca0d
Jefry <49569162+jefryelb@users.noreply.github.com>
2025-03-05 16:00:37 +0100

    Update setup.sh

--- Labs/05/setup.sh
+++ Labs/05/setup.sh
@@ -8,7 +8,7 @@ suffix=${suffix:0:18}
 # Set the necessary variables
 RESOURCE_GROUP="rg-dp100-l${suffix}"
 RESOURCE_PROVIDER="Microsoft.MachineLearningServices"
-REGIONS=("eastus" "westus" "centralus" "northeurope" "westeurope")
+REGIONS=("northeurope" "westeurope")
 RANDOM_REGION=${REGIONS[$RANDOM % ${#REGIONS[@]}]}
 WORKSPACE_NAME="mlw-dp100-l${suffix}"
 COMPUTE_CLUSTER="aml-cluster"
@@ -28,7 +28,7 @@ az configure --defaults workspace=$WORKSPACE_NAME
 
 # Create compute cluster
 echo "Creating a compute cluster with name: " $COMPUTE_CLUSTER
-az ml compute create --name ${COMPUTE_CLUSTER} --size STANDARD_DS11_V2 --max-instances 2 --type AmlCompute 
+az ml compute create --name ${COMPUTE_CLUSTER} --size STANDARD_E2AS_V4 --max-instances 2 --type AmlCompute 
 
 # Create data asset
 echo "Creating a data asset with name: diabetes-folder"
